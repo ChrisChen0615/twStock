57a75f8a510936b6366b88e9e595de571bc2b7d5
joychen <chrisjoy1864@hotmail.com>
2018-10-02 10:19:05 +0800

    修正五大、十大交易人留倉爬蟲url和參數

--- StockDaily.py
+++ StockDaily.py
@@ -8,7 +8,7 @@ dateList = ['20181001']
 
 # 大盤excel
 #五大 十大留倉數值有問題
-#StockMarketExport.main(dateList)
+StockMarketExport.main(dateList)
 
 # 上市外資(含陸資)、投信買賣超前三十
 twse = TWSEBuySell.TWSE(dateList)
--- package/ScrapySource/FiveAndTen.py
+++ package/ScrapySource/FiveAndTen.py
@@ -3,27 +3,30 @@ import requests
 from bs4 import BeautifulSoup
 from package.Infrastructure import DateObj
 
+
 class FiveAndTen:
-    def __init__(self,obj):
+    def __init__(self, obj):
         self.dataDate = obj
-        self.five_all = 0 #前五大交易人留倉部位(所有期約)
-        self.ten_all = 0 #前十大交易人留倉部位(所有期約)
+        self.five_all = 0  # 前五大交易人留倉部位(所有期約)
+        self.ten_all = 0  # 前十大交易人留倉部位(所有期約)
 
     def CalCount(self):
-        url = 'http://www.taifex.com.tw/cht/3/getLargeTradersFutContract'
+        url = 'http://www.taifex.com.tw/cht/3/largeTraderFutQry'
         params = {
+            'contractId': 'TX',
+            'contractId2': '',
+            'datecount': '',
             'queryDate': self.dataDate.dateSlash
         }
-        result = requests.post(url, params=params)
+        result = requests.post(url, data=params)
         c = result.content  # text
         soup = BeautifulSoup(c, "html.parser")
         table = soup.find_all('table', "table_f")
         rows = table[0].find_all('tr')
         five_buy = 0  # 前五大交易人(買方)
-        five_sell = 0  # 前五大交易人(賣方)        
+        five_sell = 0  # 前五大交易人(賣方)
         ten_buy = 0  # 前十大交易人(買方)
         ten_sell = 0  # 前十大交易人(賣方)
-        
 
         # 前五大交易人
         cols = rows[5].find_all('td')
